005864f9f344ac74e7580e30da49f74ebb2fa664
Duong Vu Cong Tuan <DuongVCT.B22KH024@stu.ptit.edu.vn>
2025-10-28 16:40:19 +0700

    update subtests

--- sec1/sec1-1/hello.go
+++ sec1/sec1-1/hello.go
@@ -2,14 +2,32 @@ package sec1_1
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
+const (
+	spanish = "Spanish"
+	french  = "French"
 
-func Hello(name string) string {
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+	return helloInLanguage(language) + name
+}
+func helloInLanguage(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 func main() {
-	fmt.Println(Hello("Chris"))
+	fmt.Println(Hello("Chris", "Spanish"))
 }
