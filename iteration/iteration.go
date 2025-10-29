411799de6538a34dfe4e301fd310abf4900cc904
Duong Vu Cong Tuan <DuongVCT.B22KH024@stu.ptit.edu.vn>
2025-10-29 10:12:01 +0700

    iteration playground

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,9 +1,13 @@
 package iteration
 
-func Repeat(character string) string {
-	var repeated string
-	for i := 0; i < 5; i++ {
-		repeated = repeated + character
+import "strings"
+
+const repeatCount = 5
+
+func Repeat(character string, num int) string {
+	var repeated strings.Builder
+	for i := 0; i < num; i++ {
+		repeated.WriteString(character)
 	}
-	return repeated
+	return repeated.String()
 }
--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -1,17 +1,24 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
+	repeated := Repeat("a", 5)
 	expected := "aaaaa"
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
 	}
 }
-
+func ExampleRepeat() {
+	res := Repeat("a", 3)
+	fmt.Println(res)
+	// Output: aaa
+}
 func BenchmarkRepeat(b *testing.B) {
 	for b.Loop() {
-		Repeat("a")
+		Repeat("a", 5)
 	}
 }
