f081d0e93a8583084b9ede451a70f99c64b4e420
Duong Vu Cong Tuan <DuongVCT.B22KH024@stu.ptit.edu.vn>
2025-11-04 17:02:52 +0700

    pointers and errors v5

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -1,6 +1,9 @@
 package pointers
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Bitcoin int
 type Wallet struct {
@@ -19,6 +22,10 @@ func (w *Wallet) Balance() Bitcoin {
 func (b Bitcoin) String() string {
 	return fmt.Sprintf("%d BTC", b)
 }
-func (w *Wallet) Withdraw(amount Bitcoin) {
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return errors.New("cannot withdraw, insufficient funds")
+	}
 	w.balance -= amount
+	return nil
 }
