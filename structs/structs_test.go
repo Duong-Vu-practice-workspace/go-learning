c00b59585e98ab6f185ecb3507fd4b0e479982d8
Duong Vu Cong Tuan <DuongVCT.B22KH024@stu.ptit.edu.vn>
2025-11-04 14:49:07 +0700

    structs, method and interfaces v3

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -12,10 +12,22 @@ func TestPerimeter(t *testing.T) {
 	}
 }
 func TestArea(t *testing.T) {
-	recTangle := Rectangle{10.0, 10.0}
-	got := Area(recTangle)
-	want := 100.0
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+	checkArea := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
 	}
+	t.Run("rectangles", func(t *testing.T) {
+		recTangle := Rectangle{10.0, 10.0}
+		want := 100.0
+		checkArea(t, recTangle, want)
+	})
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		want := 314.1592653589793
+		checkArea(t, circle, want)
+	})
+
 }
