4888beed80b5f9902dce836870b6b0289197a9b8
Duong Vu Cong Tuan <DuongVCT.B22KH024@stu.ptit.edu.vn>
2025-11-04 14:56:20 +0700

    structs, method and interfaces v4

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -31,3 +31,18 @@ func TestArea(t *testing.T) {
 	})
 
 }
+func TestArea1(t *testing.T) {
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{10, 10}, 100.0},
+		{Circle{10}, 314.1592653589793},
+	}
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
+		}
+	}
+}
