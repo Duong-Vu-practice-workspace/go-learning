076d7f2e9a6f19fc5e146e1eaf042b568de24aaa
Duong <vucongtuanduong6@gmail.com>
2026-01-02 09:01:57 +0000

    chapter 12 - channels -5-close channels

---
+++ boot-dev/12-channels/5-close-channels/5.go
@@ -0,0 +1,24 @@
+package main
+
+func countReports(numSentCh chan int) int {
+	// ?
+	total := 0
+	for {
+		value, ok := <-numSentCh
+		if !ok {
+			break
+		}
+		total += value
+	}
+	return total
+}
+
+// don't touch below this line
+
+func sendReports(numBatches int, ch chan int) {
+	for i := 0; i < numBatches; i++ {
+		numReports := i*23 + 32%17
+		ch <- numReports
+	}
+	close(ch)
+}
