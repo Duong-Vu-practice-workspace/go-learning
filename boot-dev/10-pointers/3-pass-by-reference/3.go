3ed415ed1ff25a252ac3add380a8699aef16217f
Duong <vucongtuanduong6@gmail.com>
2026-01-02 03:43:17 +0000

    chapter 10-pointers-3-pass-by-references

---
+++ boot-dev/10-pointers/3-pass-by-reference/3.go
@@ -0,0 +1,25 @@
+package main
+
+type Analytics struct {
+	MessagesTotal     int
+	MessagesFailed    int
+	MessagesSucceeded int
+}
+
+type Message struct {
+	Recipient string
+	Success   bool
+}
+
+// don't touch above this line
+
+// ?
+func analyzeMessage(analytics *Analytics, message Message) {
+	if message.Success {
+		analytics.MessagesTotal++
+		analytics.MessagesSucceeded++
+	} else {
+		analytics.MessagesTotal++
+		analytics.MessagesFailed++
+	}
+}
